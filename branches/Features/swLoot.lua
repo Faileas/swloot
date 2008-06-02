2caf321f823c45aeaa24e56cc48e744f02e9c941
dennis.zickefoose <dennis.zickefoose@a5379696-d047-0410-8e49-313f73787c97>
2008-06-02 18:18:36 +0000

    Turn off beta flags

--- branches/Features/swLoot.lua
+++ branches/Features/swLoot.lua
@@ -532,10 +532,10 @@ swLoot = LibStub("AceAddon-3.0"):NewAddon("swLoot", "AceConsole-3.0", "AceEvent-
                                                      "AceSerializer-3.0")
 
 swLoot.version = tonumber(strmatch("$Revision$", "%d+"))
-swLoot.reqVersion = swLoot.version --Beta release; no talking with old betas
+swLoot.reqVersion = 36
 
 --Used by the aceComm library.  Do not change without a really good reason.
-swLoot.commPrefix = "swLootBeta"
+swLoot.commPrefix = "swLoot"
 
 swLoot.altPattern = "%(Alt%)%s*(%a+)"
 
