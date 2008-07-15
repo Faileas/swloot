711110521f5b5a7558c1b9b1b4e64a4dc2ab02f3
dennis.zickefoose <dennis.zickefoose@a5379696-d047-0410-8e49-313f73787c97>
2008-07-15 02:35:15 +0000

    AwardItemToPlayer --> AwardToPlayer

--- swLootGUI.lua
+++ swLootGUI.lua
@@ -227,14 +227,14 @@ local function CreateInternalWindow(lootslot, parent)
     AttachMouseoverText(useNeed, "Use need?", 1, 1, 1, 1)
     frame.useNeed = useNeed
     
-    frame.AwardItemToPlayer = function(self, player, item)
+    frame.AwardToPlayer = function(self, player, item)
         local need = self.useNeed:GetChecked()
         if need == 1 then
             need = "need"
         else
             need = "greed"
         end
-        swLoot:AwardItem(name .. " " .. item .. " " .. need)
+        swLoot:AwardItem(player .. " " .. item .. " " .. need)
     end
     
     frame:SetWidth(useNeed:GetRight() - need:GetLeft())
